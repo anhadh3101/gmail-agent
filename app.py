ab65339b7ee0143551788b3f69401d1d62ca7ddb
anhadh3101 <anhadhsran3101@gmail.com>
2025-08-16 20:12:55 -0700

    test backend api

--- app.py
+++ app.py
@@ -1,10 +1,66 @@
-from flask import Flask
+from ast import main
+import os.path
 
-app = Flask(__name__)
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-@app.route("/")
-def home():
-    return "Email Agent Backend is running!"
+"""
+Desktop application to run in background for fetching all emails.
+
+1. Make desktop credentials on GCP and fix any configuration issues.
+2. Get the APIs to develop the workflow for the app.
+3. Develop the workflow to get the emails that were recieved in the last 24 hours or till some pivot point.
+4. Figure out things we are going to configure on LangChain. (That is going to be the AI interface where emails are handled)
+5. The flask app will help manage the information.
+
+Need to build a frontend interface for getting the data.
+"""
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+def main():
+  """Shows basic usage of the Gmail API.
+  Lists the user's Gmail labels.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    # Call the Gmail API
+    service = build("gmail", "v1", credentials=creds)
+    results = service.users().labels().list(userId="me").execute()
+    labels = results.get("labels", [])
+
+    if not labels:
+      print("No labels found.")
+      return
+    print("Labels:")
+    for label in labels:
+      print(label["name"])
+
+  except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f"An error occurred: {error}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
